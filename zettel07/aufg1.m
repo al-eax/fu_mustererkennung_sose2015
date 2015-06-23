3bfc11950f3a6fcedb8ba61e211b0fca2f1b6234
alex <hinze.alex@googlemail.com>
2015-06-23 02:13:59 +0200

    wip

--- zettel07/aufg1.m
+++ zettel07/aufg1.m
@@ -1,15 +1,13 @@
 data = importdata('/home/alex/fu_mustererkennung_sose2015/zettel07/fieldgoal.txt');
-[h,w] = size(data);
-b = [0;0];
 
-function e = error(data,beta)
+function e = error(beta,data)
   [h,w] = size(data);
   e = 0;
   for i = 1:h
     x = data(i,1);
-    y = data(i,2)
+    y = data(i,2);
     extendedX = [1;x];
-    expected = p(extendedX,beta)
+    expected = p(extendedX,beta);
     e += abs(y - expected);
   endfor
 endfunction
@@ -18,28 +16,27 @@ function w = p(x,b)
    w = 1 - 1/(1+exp(transpose(b)*x));
 endfunction
 
-function b = lBeta(x,y, beta)
-  extendedX = [1;x];
-  b = extendedX * (y - p(extendedX ,beta));
-endfunction
-
-function l = nambla(beta, data)
+function b = lBeta(beta,data)
+  b = 0;
   [h,w] = size(data);
-  l = [0;0];
   for i = 1:h
     x = data(i,1);
     y = data(i,2);
-    beta = lBeta(x,y,beta);
+    extendedX = [1;x];
+    b += extendedX*(y- p(extendedX,beta));
   endfor
 endfunction
+
+function b = nextB(beta, data)
+  a = 10^(-7);
+  b = beta + a * lBeta(beta,data);
+endfunction
+
+[h,w] = size(data);
+b = [0;0];
 
 for k = 1:100
-  bb = b;
-  for i = 1:h
-    x = data(i,1);
-    y = data(i,2);
-    b += lBeta(x,y,bb);
-  endfor
-  error(data,b);
+  b = nextB(b,data);
+  error(b,data)
 endfor
 
